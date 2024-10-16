4d829040764ad7778c646dc92230f1d6c9c35029
Mia Desiree Aburto <maburto@uci.edu>
2024-10-16 22:55:04 +0000

    fixed header file

--- include/gen_number.h
+++ include/gen_number.h
@@ -1,4 +1,8 @@
 // complete the header file
+
+#ifndef GEN_NUMBER_H
+#define GEN_NUMBER_H
+
 int generateNum();
 int doubleNum(int num);
 int reverseDigit(int num);
@@ -6,3 +10,6 @@ int sumDigits(int num);
 int raisePow2_3_4(int num, int exp);
 int raiseFirstDigitPowSecondDigit(int num);
 int raiseFirstAndSecondDigitPowThirdDigit(int num);
+
+
+#endif
