c0e37f0494b14cad4ec83ee46ff1a04631c2ad7e
Mia Desiree Aburto <maburto@uci.edu>
2024-10-17 00:10:22 +0000

    fixed random I think

--- src/gen_number.cpp
+++ src/gen_number.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 int generateNum(){
-    srand(time(0)); //seed: https://www.digitalocean.com/community/tutorials/random-number-generator-c-plus-plus#importance-of-a-seed-value
+    //srand(time(0)); //seed: https://www.digitalocean.com/community/tutorials/random-number-generator-c-plus-plus#importance-of-a-seed-value
     return rand() % 90 + 10;  // creates number between 10 and 99
 }
 
